bdc8f9c313302a0714516f3f3b68f01185648e13
tmhglnd <timohoogland@gmail.com>
2024-12-29 14:05:28 +0100

    added options for using timestamp in filename, specify length for piece, and use files without timestamps

--- README.md
+++ README.md
@@ -32,7 +32,7 @@ Install the node modules
 
 ## Usage
 
-Create a folder with Mercury Log Files you like to replay. If you don't have any log files, you can create them by turning on `Auto Log Sketch` in the Mercury window, and start performing. The player looks at the `date created` info in all the files and based on that determines when the next file should be executed. In general a Mercury log file also includes the the time in the filename, for example: `sketch_2024.11.24_00.13.20.txt`.
+Create a folder with Mercury Log Files you like to replay. If you don't have any log files, you can create them by turning on `Auto Log Sketch` in the Mercury window, and start performing. The player looks at the `date created` info in all the files and based on that determines when the next file should be executed. In general a Mercury log file also includes the the time in the filename, for example: `sketch_2024.11.24_00.13.20.txt`, but this timestamp is not needed when using the date created.
 
 In the repository run:
 
@@ -45,6 +45,14 @@ Optionally you can adjust the playback rate:
 `$ node log-player.js --folder <path> --rate 4`
 
 This will play the files back 4x faster, so if the original performance was 20 minutes, this will be 5 minutes.
+
+To disable the usage of "date created" info in the file and use your custom timestamps in the form of: `yyyy.mm.dd_hh.mm.ss` run:
+
+`$ node log-player.js --folder <path> --dateCreated=0` or `-d 0`
+
+When using filenames that don't have any timestamp, and you also don't want to use the "date created" info, you can specify a total length for the piece in `seconds` and the total time will be divided by the number of files and the evaluation-time will be split evenly.
+
+`$ node log-player.js --folder <path> --dateCreated=false --length=240` or `-l 240`
 
 Shorthand for options:
 
--- log-player.js
+++ log-player.js
@@ -11,14 +11,18 @@
 
 const fs = require('fs');
 const fg = require('fast-glob');
+const path = require('path');
 
 const { program } = require('commander');
 const { Client } = require('node-osc');
+const { log } = require('console');
 
 // the program options
 program
 	.option('-f --folder <path>')
-	.option('-r --rate <playback rate>');
+	.option('-r --rate <playback rate>')
+	.option('-l --length <specify length in seconds>')
+	.option('-d --dateCreated <use date created instead of filename>');
 
 // parse the arguments
 program.parse(process.argv);
@@ -34,9 +38,13 @@ if (process.argv.length < 3) {
 	
 	// set the playbackrate (1 = same as performance, 2 = 2x faster)
 	options.rate = options.rate ? options.rate : 1;
+	options.length = options.length ? options.length : null;
+	options.dateCreated = options.dateCreated ? parseInt(options.dateCreated) : 1;
 	
 	console.log('--folder:', options.folder);
 	console.log('--rate:', options.rate);
+	console.log('--length:', options.length);
+	console.log('--dateCreated:', options.dateCreated);
 	
 	if (!options.folder){
 		console.error('please provide a folder path with option -f');
@@ -76,6 +84,7 @@ function getFiles(path) {
 	// get the files from the path
 	let logs = fg.globSync(`${path}/**/*.txt`);
 	// sort them ascending order based on the filename
+	// even if the file doesn't contain a timestamp it is sorted as well
 	logs.sort();
 	console.log(`found files: ${logs.length}`);
 
@@ -99,19 +108,55 @@ function processFiles(logs) {
 			wait: undefined
 		});
 
+		// get the date from the filename
+		let file = path.parse(logs[i]);
+		// JS Date string format: YYYY-MM-DDTHH:mm:ss.sssZ
+		// Also works with format: YYYY.MM.DD_HH.mm.ss
+		
+		let d = file.name.match(/(\d+)/g);
+		let logtime = '';		
+		if (d){
+			d.push('000');
+			logtime = `${d[0]}-${d[1]}-${d[2]}T${d[3]}:${d[4]}:${d[5]}.${d[6]}`;
+			logtime = Date.parse(logtime);
+		}
+
+		// if the timestamp is invalid or no stamp was detected use the
+		// the specified option --length= in seconds
+		if (isNaN(logtime) || !d){
+			console.log('no time stamp in filename detected, using --length instead');
+			if (options.length === null){
+				console.error('specify length in seconds when not using timestamps');
+				process.exit();
+			}
+		}
+
 		// get the stats for date created
 		let stats = fs.statSync(logs[i]);
 		let wait = 0;
 
 		if (_time > 0) {
-			wait = stats.birthtime - _time;
+			// if no total length specified
+			if (!options.length){
+				// if using the date created from the file stats
+				if (options.dateCreated){
+					wait = stats.birthtime - _time;
+				} else {
+					console.log('using logtime');
+					wait = logtime - _time;
+				}
+			} else {
+				// if using a total time and no timestamps divide evenly
+				wait = Math.floor((options.length * 1000) / logs.length);
+			}
+			console.log('wait', wait);
+
 			playfiles[i - 1].time = _time;
 			playfiles[i - 1].wait = wait;
 		}
 
 		_total += wait;
-		_time = stats.birthtime;
-		// console.log(logs[i], _time, wait);
+		_time = options.dateCreated ? stats.birthtime : logtime;
 	}
 
 	// print the total runtime in min:sec:ms
